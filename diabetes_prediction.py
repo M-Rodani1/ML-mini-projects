603516c9be056deba60d223677a18c91b2e88b66
Mohamed Rodani <your-email@example.com>
2026-01-25 18:28:47 +0000

    add data splitting

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -17,4 +17,3 @@ y = df['Outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42,stratify=y)
 print('split complete')
 print(f"Training Data Shape: {X_train.shape}")
-print(f"Test Data Shape:     {X_test.shape}")
