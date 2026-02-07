bc49ad67c55122aad023d6706cc3870b8f6680d0
Mohamed Rodani <rodani484@gmail.com>
2026-02-07 16:15:12 +0000

    Implement car price prediction model with evaluation
    Added data preprocessing, model training, and evaluation for car price prediction.

--- car-price-prediction/main.py
+++ car-price-prediction/main.py
@@ -44,3 +44,40 @@ df = df.reset_index(drop=True)
 print("\nCleaning Verification")
 print(df.info())
 print(f"Remaining Rows: {len(df)}")
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import r2_score
+
+#  Define X and y
+X = df.drop(columns=['Price'])
+y = df['Price']
+
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=433)
+
+one = OneHotEncoder()
+
+one.fit(X[['name', 'company', 'fuel_type']])
+
+#  Create Column Transformer
+column_trans = make_column_transformer(
+    (OneHotEncoder(categories=one.categories_), ['name', 'company', 'fuel_type']),
+    remainder='passthrough'
+)
+
+# Pipeline
+lr = LinearRegression()
+pipe = make_pipeline(column_trans, lr)
+
+#  Train
+pipe.fit(X_train, y_train)
+
+# Evaluate
+y_pred = pipe.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+
+print(f"R2 Score: {r2:.4f}")
